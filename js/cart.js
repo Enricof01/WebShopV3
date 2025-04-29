46fcc2bf494ac120f2a2498fc3ce3c2f43eb2085
Enrico Fritz <fritz.enrico@gmx.de>
2025-04-29 13:42:56 +0200

    JS datei überarbeitet

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,80 @@
-let cart = []
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 function add(event){
     console.log('button wurde geklickt' + event.target.id)
     
     let newItem = { id: 1, name: event.target.id, quantity: 1, price: "14,99"};
     cart.push(newItem);
-    localStorage.setItem('cart', JSON.stringify('cart'));
+    localStorage.setItem('cart', JSON.stringify(cart));
 
+    let currentCart = JSON.parse(localStorage.getItem('cart'));
+    console.log(currentCart);
     cart.forEach(item =>{
         console.log(item)
     });
 
+}
+
+// function updateCart(){
+//     const cartArea = document.querySelector(".cartBody")
+
+//     console.log("updated")
+
+//     cart.forEach((item, index) =>{
+//         let productRow = document.createElement('div')
+//         productRow.classList.add('row')
+//         productRow.classList.add(`product${index}`)
+
+//         let productCol = document.createElement('div')
+//         productCol.classList.add('col-2')
+
+
+//         const line = document.createElement('hr')
+//         cartArea.appendChild(line)
+//         cartArea.appendChild(productRow)
+//         productRow.appendChild(productCol)
+
+//         console.log(productRow.className)
+//     })
+
+// }
+
+function updateCart2(){
+    const cartArea = document.querySelector(".cartBody")
+
+    cart.forEach((item,index) =>{
+        cartArea.innerHTML += `<div class = "row product1">
+        <div class = "col-2">
+
+            <div class = "productImage">
+                <img src="https://placehold.co/150x150" alt="" class = "img-fluid">
+            </div>
+
+        </div>
+
+        <div class = "col-5">
+            <p>Prodcut description and more details</p>
+            <select class="form-select" aria-label="Default select example" style = "width: 20%;">
+                <option selected>1</option>
+                <option value="1">2</option>
+                <option value="2">3</option>
+                <option value="3">4</option>
+              </select>
+              <div style="margin-top: 1%;">
+                <button onclick = "updateCart();"type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button>
+
+              </div>
+        </div>
+
+        <div class = "col-5 price">
+            <div class = "productPrice">
+                <p>29,99</p>
+            </div>
+
+        </div>
+
+    </div>
+        
+        `;
+    })
 }
